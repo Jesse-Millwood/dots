0ba1d88173e25a6a3579ffb8bcde98b32cda340e
Jesse Millwood <jesse.millwood@dornerworks.com>
2019-12-15 09:32:23 -0500

    [Emacs] Added OCaml packages

--- dot_emacs.d/init.el
+++ dot_emacs.d/init.el
@@ -653,6 +653,40 @@
   (python-indent-offset 4)
   )
 
+(use-package tuareg
+  :ensure t
+  :custom
+  (tuareg-match-patterns-aligned t)
+  :config
+  ;; Setup environment variables using opam
+  (dolist
+      (var (car (read-from-string
+                 (shell-command-to-string "opam config env --sexp"))))
+    (setenv (car var) (cadr var)))
+  ;; Update the emacs path
+  (setq exec-path (split-string (getenv "PATH") path-separator))
+  ;; Update the emacs load path
+  (push (concat (getenv "OCAML_TOPLEVEL_PATH")
+                "/../../share/emacs/site-lisp") load-path)
+  )
+
+(use-package merlin
+  :ensure t
+  :defer t
+  :config
+  (add-hook 'tuareg-mode-hook 'merlin-mode)
+  (with-eval-after-load 'company
+    (add-to-list 'company-backends 'merlin-company-backend))
+  (add-hook 'merlin-mode-hook 'company-mode)
+  )
+
+;; (use-package dune
+;;   :ensure nil
+;;   :mode "\dune\\'"
+;;   :load-path (lambda () (concat (getenv "OCAML_TOP_LEVEL_PATH")
+;;                                 "../../share/emacs/site-lisp"))
+;;   )
+
 ;; Reduce gc threshold
 (setq gc-cons-threshold (* 2 1000 1000))
 
