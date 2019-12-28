5eabad545080e6bd24af9712dfd8637c06e2172c
Jesse Millwood <jesse.c.millwood@gmail.com>
2019-12-27 23:38:50 -0500

    [Emacs] Fixed displaying splash screen

--- dot_emacs.d/splash/splash.el
+++ dot_emacs.d/splash/splash.el
@@ -22,23 +22,44 @@
   :type 'string
   :group 'splash)
 
+(defun input-centered-string (string)
+  "Input the STRING with space offsets to be centered"
+  (let* ((indent-columns (floor (- screen-width (length splash-title)) 2)))
+    (insert (format "%s%s" (make-string indent-columns ? ) string))
+    ))
+
 (defun get-splash-buffer ()
-  (let* ( (splash-buf (generate-new-buffer "*Splash*"))
-          (image-spec (create-image splash-image
+  (let* ( (splash-buf (generate-new-buffer "*Splash*")) )
+    (with-current-buffer splash-buf
+      (let* (
+             (image-spec (create-image splash-image
                                        'imagemagick nil
                                        :scale 0.25
                                        :background nil))
-          (screen-width (window-width))
-          (image_size (image-size image-spec))
-          (image_width (car image_size))
-          )
-    (with-current-buffer splash-buf
-      (let* (
-             (left-margin-width (floor (- screen-width image_width) 2))
-             (display-numbers nil)
+             (screen-width (window-width))
+             (image_size (image-size image-spec nil))
+             (image_width (car image_size))
+             (frame-type (framep (window-frame)))
+             (image-indent-columns (floor (- screen-width image_width) 2) )
+;;             (left-margin-width image-indent-columns)
+             ;;             (left-margin image-indent-columns)
+             (left-margin-width 0)
+             (right-margin-width 0)
+             (display-line-numbers nil)
              )
-        (insert (format "%s\n\n" splash-title))
-        (insert-image image-spec)
+        (fundamental-mode)
+        (input-centered-string splash-title)
+        (insert "\n\n")
+        (cond ((string= frame-type "t") (insert "image here")
+               )
+              ((string= frame-type "x")
+               (progn
+                 (insert (make-string image-indent-columns ? ))
+                 (insert-image image-spec)
+                 (insert "\n")
+                 )
+               )
+              )
         (insert "\n\n")
         ;; (read-only-mode)
         )
