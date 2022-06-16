045433bf436042b9e31f86d2f209e838308aaf5d
Jesse Millwood <jcmillwo@WFLWL0319090209.ndc.nasa.gov>
2022-06-16 15:43:52 -0400

    Updated the spaceship install script to check for needed files

--- .chezmoiscripts/run_finish_spaceship_install.sh
+++ .chezmoiscripts/run_finish_spaceship_install.sh
@@ -6,6 +6,13 @@ export ZDOTDIR=${ZDOTDIR:=${XDG_CONFIG_HOME}/zsh}
 export ZSH=${ZDOTDIR}/oh-my-zsh
 export ZSH_CUSTOM=${ZSH}/custom
 
-if [ ! -f $ZSH_CUSTOM/themes/spaceship.zsh-theme ]; then
-    ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+if [ -f "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" ]; then
+    # Check for the spaceship installation and only if that is installed
+    # finish the install for the spaceship theme
+    # On the first run of the system this might run before the spaceship
+    # theme is installed
+    if [ ! -f $ZSH_CUSTOM/themes/spaceship.zsh-theme ]; then
+        ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" \
+           "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+    fi
 fi
