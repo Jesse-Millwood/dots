38be975ae9c5a44fd997fadb6a9553a1ce1e8157
Jesse Millwood <jesse.c.millwood@nasa.gov>
2023-03-02 10:21:30 -0500

    [Emacs] Update eglot configuration
    - Remove which-func setup
    - Add c++-mode and csharp-mode to hook
    - Add Packages
     - flycheck-eglot

--- private_dot_config/emacs/eglot-setup.el
+++ private_dot_config/emacs/eglot-setup.el
@@ -3,13 +3,11 @@
 (use-package eglot
   :hook ((eglot-managed-mode . company-mode)
          (c-mode . eglot-ensure)
-         (c++-mode . eglot-ensure))
-  :config
-  (which-function-mode)
-  (setq-default header-line-format
-                '((which-func-mode ("" which-func-format " "))))
-  (setq mode-line-misc-info
-        ;; We remove Which Function Mode from the mode line, because it's mostly
-        ;; invisible here anyway.
-        (assq-delete-all 'which-func-mode mode-line-misc-info))
+         (c++-mode . eglot-ensure)
+         (csharp-mode . eglot-ensure))
   )
+
+(use-package flycheck-eglot
+  :after (flycheck eglot)
+  :config
+  (global-flycheck-eglot-mode 1))
