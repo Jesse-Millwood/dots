a0563cafdf17719ed07ffc85547bc428d7a7b4cf
Jesse Millwood <jmillwood@masten.aero>
2023-03-02 13:21:34 -0500

    [Emacs] Update init.el
    - Add use-package form for tramp
    - Add packages
     - multi-vterm
     - ox-hugo
     - discover
     - mastodon
     - org-noter
     - org-pdftools

--- private_dot_config/emacs/init.el
+++ private_dot_config/emacs/init.el
@@ -132,6 +132,9 @@
   (dired-listing-switches "-alFh")
   )
 
+(use-package tramp
+  :custom (tramp-terminal-type "tramp"))
+
 (use-package auth-source
   :config
   (let ((potential-auth-sources '("~/.config/auth/authinfo.gpg"
@@ -896,6 +899,26 @@
 
 (use-package dired-rsync)
 
+(use-package multi-vterm)
+
+(use-package ox-hugo
+  :after ox)
+
+(use-package discover)
+
+(use-package mastodon
+  :pin  melpa
+  :custom
+  (mastodon-instance-url "https://fosstodon.org")
+  (mastodon-active-user "@jesse_m")
+  (mastodon-tl--show-avatars t)
+  :config
+  (setq mastodon)
+  (require 'mastodon-async)
+  (require 'discover)
+  (mastodon-discover)
+  )
+
 (use-package pdf-tools
   ;; Un-pin this when first installing
   :disabled
@@ -924,6 +947,42 @@
         ;; kind of useless
         (assq-delete-all 'which-func-mode mode-line-misc-info))
   )
+
+(use-package org-noter)
+(use-package org-pdftools
+  :hook (org-mode . org-pdftools-setup-link))
+(use-package org-noter-pdftools
+  :after org-noter
+  :config
+  ;; Add a function to ensure precise note is inserted
+  (defun org-noter-pdftools-insert-precise-note (&optional toggle-no-questions)
+    (interactive "P")
+    (org-noter--with-valid-session
+     (let ((org-noter-insert-note-no-questions (if toggle-no-questions
+                                                   (not org-noter-insert-note-no-questions)
+                                                 org-noter-insert-note-no-questions))
+           (org-pdftools-use-isearch-link t)
+           (org-pdftools-use-freepointer-annot t))
+       (org-noter-insert-note (org-noter--get-precise-info)))))
+
+  ;; fix https://github.com/weirdNox/org-noter/pull/93/commits/f8349ae7575e599f375de1be6be2d0d5de4e6cbf
+  (defun org-noter-set-start-location (&optional arg)
+    "When opening a session with this document, go to the current location.
+With a prefix ARG, remove start location."
+    (interactive "P")
+    (org-noter--with-valid-session
+     (let ((inhibit-read-only t)
+           (ast (org-noter--parse-root))
+           (location (org-noter--doc-approx-location (when (called-interactively-p 'any) 'interactive))))
+       (with-current-buffer (org-noter--session-notes-buffer session)
+         (org-with-wide-buffer
+          (goto-char (org-element-property :begin ast))
+          (if arg
+              (org-entry-delete nil org-noter-property-note-location)
+            (org-entry-put nil org-noter-property-note-location
+                           (org-noter--pretty-print-location location))))))))
+  (with-eval-after-load 'pdf-annot
+    (add-hook 'pdf-annot-activate-handler-functions #'org-noter-pdftools-jump-to-note)))
 
 ;; Conditionally Load OCaml files
 (add-to-list 'auto-mode-alist '("\.\(ml\|mli\)\\'" .  (lambda ()
